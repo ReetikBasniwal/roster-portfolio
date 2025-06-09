8e7908660d1110beebf7e26988004c4b8f817137
Reetik Basniwal <reetikbasniwal@gmail.com>
2025-06-09 16:38:53 +0530

    remove unsed code

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Button } from './ui/button'
 import { useAppDispatch, useAppSelector } from '@/hooks/reduxHooks'
 import BasicInfoSection from './BasicInfoSection'
